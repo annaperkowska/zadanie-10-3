1af38de49b192c25659a67478585c617a8981880
Anna Perkowska <perkowska.a@gmail.com>
2018-02-09 10:47:02 +0100

    Change script

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ $(function() {
 	var carouselList = $("#carousel ul"); 
 });
 
-var image = $("#carousel ul li");
-
 function changeImage() {
 	carouselList.animate({marginLeft: -400}, 500, function moveFirstSlide () {
 		var firstItem = carouselList.find("li:first");
